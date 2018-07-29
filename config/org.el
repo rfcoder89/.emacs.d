3d52eff2812f416ca19257332accf1364ea42b91
Richard Flood <rfcoder89@gmail.com>
2018-07-29 11:17:46 +0100

    org global key bindings

--- config/org.el
+++ config/org.el
@@ -2,4 +2,12 @@
   :config
   (setq org-log-done t)
   (setq org-agenda-files '("~/.emacs.d/docs"))
-  (set-face-foreground 'org-level-1 "DeepSkyBlue"))
+  (set-face-foreground 'org-level-1 "DeepSkyBlue")
+
+  (bind-keys
+   :prefix-map rlf/p/org
+   :prefix "C-x C-o"
+   ("C-o" . org-agenda)
+   ("C-c" . org-capture)
+   ("C-l" . org-store-link)
+   ("C-b" . org-switchb)))
