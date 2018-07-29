a4e50801a3229bdda81ea5fbffbf7ac4a499b7f0
Richard Flood <rfcoder89@gmail.com>
2018-07-29 12:42:41 +0100

    configured webmarks, webmarks.json added

--- config/bookmarks.el
+++ config/bookmarks.el
@@ -3,3 +3,13 @@
 (define-key rlf/prefix/bookmark (kbd "B") 'bookmark-set)
 (define-key rlf/prefix/bookmark (kbd "N") 'bookmark-delete)
 (define-key rlf/prefix/bookmark (kbd "X") 'rlf-bookmarks-clear)
+
+(use-package webmarks
+  :after ivy
+  :load-path (lambda () (path-join rlf-modules "webmarks"))
+  :config
+  (setq webmarks-file "~/.emacs.d/webmarks.json")
+  (webmarks-reload)
+  
+  :bind
+  ("C-x C-n" . webmarks-list))
