b6b3bcd891bf5f0d261d8574bff4732595ee5eda
Richard Flood <Richard.Flood@aveva.com>
2018-08-08 13:14:04 +0100

    improved compilation-mode. moving to/from errors only follows

--- config/compilation.el
+++ config/compilation.el
@@ -1,7 +1,14 @@
 (defun rlf-compilation-mode-keys ()
   "My keys for `compilation-mode'."
   (interactive)
-  (local-set-key (kbd "p") 'previous-error)
-  (local-set-key (kbd "n") 'next-error))
+  (local-set-key (kbd "p") '(lambda ()
+                              (interactive)
+                              (compilation-previous-error 1)
+                              (compilation-display-error)))
+                 
+  (local-set-key (kbd "n") '(lambda ()
+                              (interactive)
+                              (compilation-next-error 1)
+                              (compilation-display-error))))
 
 (add-hook 'compilation-mode-hook 'rlf-compilation-mode-keys)
