e5940e732c5001f3d62b07fc5ca3364b64005588
Richard Flood <Richard.Flood@aveva.com>
2018-08-08 13:14:33 +0100

    added some dired jump hotkeys

--- config/dired.el
+++ config/dired.el
@@ -17,6 +17,9 @@
   
   (:map dired-mode-map
 			  ("f" . dired-mark-files-regexp)
+			  ("e" . (lambda () (interactive) (dired "~/.emacs.d")))
+			  ("h" . (lambda () (interactive) (dired "~")))
+        ("r" . (lambda () (interactive) (dired "~/development")))
 			  ("M-o" . dired-omit-mode)
 			  ("C-M-n" . dired-find-file)
 			  ("C-M-p" . dired-up-directory)
