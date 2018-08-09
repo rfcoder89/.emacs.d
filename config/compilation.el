a275002eb38fa60b033751006c0511301249bed3
Richard Flood <Richard.Flood@aveva.com>
2018-08-09 14:31:34 +0100

    fixed colour in compilation mode and hotkey to switch to mocha error syntax

--- config/compilation.el
+++ config/compilation.el
@@ -1,6 +1,7 @@
 (defun rlf-compilation-mode-keys ()
   "My keys for `compilation-mode'."
   (interactive)
+  (local-set-key (kbd "m") 'mocha-compilation-mode)
   (local-set-key (kbd "p") '(lambda ()
                               (interactive)
                               (compilation-previous-error 1)
@@ -12,3 +13,11 @@
                               (compilation-display-error))))
 
 (add-hook 'compilation-mode-hook 'rlf-compilation-mode-keys)
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
