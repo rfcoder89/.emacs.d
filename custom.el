c6b2c7e1eb0717fe3cd799e4133fb4e97f6994c0
Richard Flood <Richard.Flood@aveva.com>
2018-08-09 14:33:37 +0100

    added some customisations

--- custom.el
+++ custom.el
@@ -1,14 +1,18 @@
-(custom-set-variables
+asy jump to compilation buffer(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coverlay:tested-line-background-color "")
+ '(mocha-command
+   "node_modules/mocha/bin/_mocha --require babel-core/register")
+ '(mocha-reporter "scripts/mocha-emacs-reporter.js")
  '(package-selected-packages
    (quote
-    (ivy-yasnippet js2-refactor rjsx-mode json-mode counsel-tramp counsel-projectile counsel ivy smex diff-hl magit company popwin dakrone-theme use-package))))
+    (coverlay mocha typescript-mode ivy-yasnippet js2-refactor rjsx-mode json-mode counsel-tramp counsel-projectile counsel ivy smex diff-hl magit company popwin dakrone-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:foreground "DeepSkyBlue" :weight bold :height 1)))))
