246524ffd949370f572fb27922e7f80ab68b7117
Richard Flood <Richard.Flood@aveva.com>
2018-08-17 20:01:18 +0100

    added telephone-line (modeline)

--- config/visual.el
+++ config/visual.el
@@ -41,7 +41,24 @@
   :config
   (load-theme 'dakrone t))
 
-(set-face-background 'cursor "DeepSkyBlue")
+(use-package telephone-line
+  :ensure t
+  :config
+  (setq telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-vc-segment
+                     telephone-line-erc-modified-channels-segment
+                     telephone-line-process-segment))
+          (nil    . (telephone-line-minor-mode-segment
+                     telephone-line-buffer-segment))))
+
+  (setq telephone-line-rhs
+        '((nil    . (telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment))
+          (evil   . (telephone-line-airline-position-segment))))
+  (telephone-line-mode 1))
 
 (diminish 'eldoc-mode)
+
+(set-face-background 'cursor "DeepSkyBlue")
 (set-default-font "Input-10")
