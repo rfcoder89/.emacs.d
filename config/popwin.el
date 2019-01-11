618a33f38fe3226a650c2fdbe5ddb6d13a985fae
Richard.Flood <Richard.Flood@aveva.com>
2019-01-11 23:58:36 +0000

    popwin changes

--- config/popwin.el
+++ config/popwin.el
@@ -6,6 +6,6 @@
   (push "*eshell*" popwin:special-display-config)
   (push '("\*Skeletor.*\*$" :regexp t :noselect t) popwin:special-display-config)
   (push '("*Warnings*" :height 5 :position top) popwin:special-display-config)
-  (push '("*mocha tests*" :height 50 :position bottom) popwin:special-display-config)
-  (push '("*compilation*" :height 50 :position bottom) popwin:special-display-config)
+  (push "*mocha tests*" popwin:special-display-config)
+  (push '("*compilation*" :height 20 :position bottom) popwin:special-display-config)
   (push '("*Messages*" :height 50 :position bottom :noselect t) popwin:special-display-config))
