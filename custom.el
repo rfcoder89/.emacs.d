91f0b613b4869a579fd2538da8a1ba16aea1439f
Richard.Flood <Richard.Flood@aveva.com>
2019-01-13 09:51:46 +0000

    updated custom.el

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
  '(mocha-reporter "scripts/mocha-emacs-reporter.js")
  '(package-selected-packages
    (quote
-    (lua-mode telephone-line eslint-fix hmac coverlay mocha typescript-mode ivy-yasnippet js2-refactor rjsx-mode json-mode counsel-tramp counsel-projectile counsel ivy smex diff-hl magit company popwin dakrone-theme use-package))))
+    (yaml-tomato yaml-mode indent-tools pug-mode lua-mode telephone-line eslint-fix hmac coverlay mocha typescript-mode ivy-yasnippet js2-refactor rjsx-mode json-mode counsel-tramp counsel-projectile counsel ivy smex diff-hl magit company popwin dakrone-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
